15eb842814ce75cb26b3cec26b441a7f28b63434
spotdemo4 <me@trev.xyz>
2026-06-18 00:20:09 -0400

    chore(ci): remove github renovate workflow and config

--- flake.nix
+++ flake.nix
@@ -199,18 +199,7 @@
             '';
           };
 
-          renovate-gh = {
-            root = ./.github;
-            files = ./.github/renovate.json;
-            packages = with pkgs; [
-              renovate
-            ];
-            script = ''
-              renovate-config-validator renovate.json
-            '';
-          };
-
-          renovate-fj = {
+          renovate = {
             root = ./.forgejo;
             files = ./.forgejo/renovate.json;
             packages = with pkgs; [
