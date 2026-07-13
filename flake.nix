9338bc86d2a1a226a29205be7b6ae41e2fdecf3f
trev <me@trev.xyz>
2026-07-12 22:19:30 -0400

    chore(flake): use new mkApps syntax

--- flake.nix
+++ flake.nix
@@ -156,20 +156,12 @@
             cross-runtime-lifecycle-stress = "nix develop -c bash -c 'mkdir -p target && go build -C trevrpc-go -o ../target/trevrpc-xruntime-go ./cmd/trevrpc-xruntime-go && TREVRPC_XRUNTIME_GO=$PWD/target/trevrpc-xruntime-go cargo test --manifest-path trevrpc-rust/Cargo.toml --test cross_runtime -- --ignored cross_runtime_lifecycle_stress --nocapture'";
             dev = "cargo run --manifest-path trevrpc-rust/Cargo.toml";
             test = "nix flake check";
-          }
-          // {
             update-kotlin-deps = {
-              type = "app";
-              program = pkgs.lib.getExe (
-                pkgs.writeShellApplication {
-                  name = "update-kotlin-deps";
-                  runtimeInputs = [ pkgs.oxfmt ];
-                  text = ''
-                    ${self.packages.${system}.trevrpc-kotlin.mitmCache.updateScript}
-                    oxfmt --write trevrpc-kotlin/gradle/deps.json
-                  '';
-                }
-              );
+              packages = [ pkgs.oxfmt ];
+              script = ''
+                ${self.packages.${system}.trevrpc-kotlin.mitmCache.updateScript}
+                oxfmt --write trevrpc-kotlin/gradle/deps.json
+              '';
             };
           };
 
